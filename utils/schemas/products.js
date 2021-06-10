f450ced9a2e8d77144ec2d9a269249ce1458c02c
Mserna10 <sernamartin46@gmail.com>
2021-06-10 13:21:19 -0500

    Updating Schemas

--- utils/schemas/products.js
+++ utils/schemas/products.js
@@ -2,6 +2,10 @@ const joi = require("@hapi/joi");
 
 const productIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const productNameSchema = joi.string().max(60);
+const productBrandSchema = joi.string().max(20);
+const productClassSchema = joi.string().max(25);
+const productProductSchema = joi.string().max(50);
+const productDescriptionSchema = joi.string().max(150);
 const productPriceSchema = joi.number().min(50).max(100000);
 const productTypeSchema = joi.string().max(20);
 const productImgSchema = joi.string();
@@ -9,9 +13,13 @@ const productPiecesSchema = joi.number().max(300);
 
 const createProductSchema = {
     name: productNameSchema.required(),
-    price: productPriceSchema.required(),
-    type: productTypeSchema.required(),
+    brand: productBrandSchema.required(),
     img: productImgSchema.required(),
+    type: productTypeSchema.required(),
+    class: productClassSchema.required(),
+    product: productProductSchema.required(),
+    description: productDescriptionSchema.required(),
+    price: productPriceSchema.required(), 
     pieces: productPiecesSchema.required()
 };
 
